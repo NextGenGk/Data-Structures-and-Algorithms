b2cbe4cd69c9d5f94458383d7e824dd6f3675c54
CG-DTE <atkola12345@gmail.com>
2023-07-13 12:35:20 +0530

    Add Stock_Buy_and_Sell.java

--- Arrays/Stock_Buy_and_Sell.java
+++ Arrays/Stock_Buy_and_Sell.java
@@ -5,14 +5,48 @@ public class Stock_Buy_and_Sell {
 
     // Method 1: Brute Force
     // Time - O(N^2), Space - O(1)
+//    static int maximize (int[] arr, int n) {
+//        int maxProfit = 0;
+//        for (int i=0; i<n; i++) {
+//            for (int j=i+1; j<n; j++) {
+//                if (arr[j] - arr[i] > maxProfit) {
+//                    maxProfit = arr[j] - arr[i];
+//                }
+//            }
+//        }
+//        return maxProfit;
+//    }
+
+    // Method 2: Better Solution (Using temp array)
+    // Time - O(N), Space - O(N)
+//    static int maximize (int[] arr, int n) {
+//        int[] temp = new int[n];
+//
+//        temp[n-1] = arr[n-1];
+//
+//        for (int i=n-2; i>=0; i--) {
+//            temp[i] = Math.max(arr[i], temp[i+1]);
+//        }
+//
+//        int j = 0;
+//        int maxProfit = 0;
+//        for (int i=0; i<n; i++) {
+//            maxProfit = Math.max(maxProfit, temp[j] - arr[i]);
+//            j++;
+//        }
+//        return maxProfit;
+//    }
+
+    // Method 3 - Optimal Solution (Using minPrice and maxProfit)
+    // Time - O(N), Space - O(1)
     static int maximize (int[] arr, int n) {
+        int minPrice = arr[0];
         int maxProfit = 0;
+
         for (int i=0; i<n; i++) {
-            for (int j=i+1; j<n; j++) {
-                if (arr[j] - arr[i] > maxProfit) {
-                    maxProfit = arr[j] - arr[i];
-                }
-            }
+            minPrice = Math.min(arr[i], minPrice);
+            int profit = arr[i] - minPrice;
+            maxProfit = Math.max(maxProfit, profit);
         }
         return maxProfit;
     }
@@ -37,4 +71,29 @@ public class Stock_Buy_and_Sell {
     a. Run a loop from j = i+1 to n-1
         i. If arr[j] - arr[i] > maxProfit, then update maxProfit
 3. Return maxProfit
+ */
+
+// Algorithm: Better Solution (Using temp array)
+/*
+1. Initialize temp array of size n
+2. Initialize temp[n-1] = arr[n-1]
+3. Run a loop from i = n-2 to 0
+    a. temp[i] = max(arr[i], temp[i+1])
+4. Initialize j = 0
+5. Initialize maxProfit = 0
+6. Run a loop from i = 0 to n-1
+    a. maxProfit = max(maxProfit, temp[j] - arr[i])
+    b. j++
+7. Return maxProfit
+ */
+
+// Algorithm: Optimal Solution (Using minPrice and maxProfit)
+/*
+1. Initialize minPrice = arr[0]
+2. Initialize maxProfit = 0
+3. Run a loop from i = 0 to n-1
+    a. minPrice = min(arr[i], minPrice)
+    b. profit = arr[i] - minPrice
+    c. maxProfit = max(maxProfit, profit)
+4. Return maxProfit
  */
