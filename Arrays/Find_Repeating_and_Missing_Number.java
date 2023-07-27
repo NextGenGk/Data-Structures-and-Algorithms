08dd550da0fc78b4cbb018bbfe6d2e8bc5f2675d
NextGenGk <atkola12345@gmail.com>
2023-07-27 01:23:16 -0700

    Update Find_Repeating_and_Missing_Number

--- Arrays/Find_Repeating_and_Missing_Number.java
+++ Arrays/Find_Repeating_and_Missing_Number.java
@@ -54,11 +54,46 @@ public class Find_Repeating_and_Missing_Number {
         return ans;
     }
 
+    // Method 3 : Optimal Solution 1
+    // Time Complexity: O(N), where N = the size of the given array.
+    // Reason: We are using only one loop running for N times. So, the time complexity will be O(N).
+    // Space Complexity: O(1) as we are not using any extra space.
+    static int[] findMissingRepeatingNumbers2(int[] arr) {
+        long n = arr.length; // size of the array
+        // Find Sn and S2n:
+        long SN = (n * (n + 1)) / 2;
+        long S2N = (n * (n + 1) * (2 * n + 1)) / 6;
+
+        // Calculate S1 and S2:
+        long S1 = 0, S2 = 0;
+        for (int i = 0; i < n; i++) {
+            S1 += arr[i];
+            S2 += (long)arr[i] * (long)arr[i];
+        }
+
+        //S1-Sn = X-Y:
+        long val1 = S1 - SN;
+
+        // S2-S2n = X^2-Y^2:
+        long val2 = S2 - S2N;
+
+        //Find X+Y = (X^2-Y^2)/(X-Y):
+        val2 = val2 / val1;
+
+        //Find X and Y: X = ((X+Y)+(X-Y))/2 and Y = X-(X-Y),
+        // Here, X-Y = val1 and X+Y = val2:
+        long x = (val1 + val2) / 2;
+        long y = x - val1;
+
+        int[] ans = {(int)x, (int)y};
+        return ans;
+    }
+
 
     // Main Function
     public static void main(String[] args) {
         int[] a = {3, 1, 2, 5, 4, 6, 7, 5};
-        int[] ans = findMissingRepeatingNumbers1(a);
+        int[] ans = findMissingRepeatingNumbers2(a);
         System.out.println("The repeating and missing numbers are: {"
                 + ans[0] + ", " + ans[1] + "}");
     }
@@ -79,4 +114,47 @@ public class Find_Repeating_and_Missing_Number {
 1. The range of the number is 1 to N. So, we need a hash array of size N+1 (as we want to store the frequency of N as well).
 2. We will iterate all the elements of the given array and update the hash array accordingly i.e. hash[a[i]] = hash[a[i]]+1.
 3. Now, we will iterate on the hash array and return the two elements with frequencies 2 and 0.
+ */
+
+// Algorithm : Optimal Solution 1
+/*
+Intuition:
+The idea is to convert the given problem into mathematical equations. Since we have two variables i.e. missing and repeating, we will try to form two linear equations. And then we will find the values of two variables using those equations.
+
+Assume the repeating number to be X and the missing number to be Y.
+
+In the array, the numbers are between 1 to N, and in that range, one number is missing and one number is occurring twice.
+
+// Step 1: Form equation 1:
+
+Now, we know the summation of the first N numbers is:
+
+Sn = (N*(N+1))/2
+Let’s say, S = the summation of all the elements in the given array.
+
+Therefore, S - Sn = X - Y…………………equation 1
+// Step 2: Form equation 2:
+
+Now, we know the summation of squares of the first N numbers is:
+
+S2n = (N*(N+1)*(2N+1))/6
+Let’s say, S2 = the summation of squares of all the elements in the given array.
+
+Therefore, S2-S2n = X2-Y2...................equation 2
+From equation 2 we can conclude,
+
+X+Y = (S2 - S2n) / (X-Y) [From equation 1, we get the value X-Y] ………… equation 3
+From equation 1 and equation 3, we can easily find the value of X and Y. And this is what we want.
+
+Note: Here, we are summing all the numbers and squares of all the numbers, so we should use a bigger data type(Like in C++, long long instead of int).
+
+Approach:
+Assume the repeating number to be X and the missing number to be Y.
+
+The steps are as follows:
+
+1. First, find out the values of S and Sn and then calculate S – Sn (Using the above formulas).
+2. Then, find out the values of S2 and S2n and then calculate S2 – S2n.
+3. After performing steps 1 and 2, we will be having the values of X + Y and X – Y. Now, by substitution of values,
+we can easily find the values of X and Y.
  */
