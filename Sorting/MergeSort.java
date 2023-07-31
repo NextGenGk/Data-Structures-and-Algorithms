d53865ab2cb200177f99ca8d761b95164c1d26eb
GAURAV KUMAR <atkola12345@gmail.com>
2023-07-31 22:56:43 +0530

    Update MergeSort.java

--- Sorting/MergeSort.java
+++ Sorting/MergeSort.java
@@ -86,6 +86,10 @@ public class MergeSort {
     }
 }
 
+// Merge sort is defined as a sorting algorithm that works by dividing an array into smaller
+// subarrays, sorting each subarray, and then merging the sorted subarrays back together to 
+// form the final sorted array.
+
 // Algorithm (approach) -
 /*
 1. Start
