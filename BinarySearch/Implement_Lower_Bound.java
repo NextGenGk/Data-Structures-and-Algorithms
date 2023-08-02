42688d3ac50a79d8c117606b8fc13d0d2a24d3cb
NextGenGk <atkola12345@gmail.com>
2023-08-02 22:28:13 +0530

    Update Implement_Lower_Bound

--- BinarySearch/Implement_Lower_Bound.java
+++ BinarySearch/Implement_Lower_Bound.java
@@ -17,24 +17,25 @@ public class Implement_Lower_Bound {
 
     // Method 2 : Optimal Solution
     // Time - O(log2N), Space - O(1)
-//    static int lowerBound(int[] arr, int target) {
-//        int n = arr.length;
-//
-//        int low = 0;
-//        int high = n-1;
-//        int ans = n;
-//
-//        while (low <= high) {
-//            int mid = low + (high - low) / 2;
-//            if (arr[mid] >= target) {
-//                ans = mid;
-//                high = mid-1;
-//            }
-//            else low = mid + 1;
-//        }
-//        return ans;
-//    }
+    static int lowerBound(int[] arr, int target) {
+        int n = arr.length;
 
+        int low = 0;
+        int high = n-1;
+        int ans = n;
+
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+            if (arr[mid] >= target) {
+                ans = mid;
+                high = mid-1;
+            }
+            else low = mid + 1;
+        }
+        return ans;
+    }
+
+    // Main Function
     public static void main(String[] args) {
         int[] arr = {3, 5, 8, 15, 19};
         int x = 9;
