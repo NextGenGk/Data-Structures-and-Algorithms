9253189952ead08527388251bad2aafe135377e8
NextGenGk <atkola12345@gmail.com>
2023-08-08 18:51:42 +0530

    Add Floor_Value

---
+++ BinarySearch/Floor_Value.java
@@ -0,0 +1,55 @@
+package BinarySearch;
+
+public class Floor_Value {
+
+    // Method 1 : Optimal Solution
+    // Time - O(log2N), Space - O(1)
+    static int floor(int[] arr, int target) {
+        int n = arr.length;
+
+        int low = 0;
+        int high = n-1;
+        int ans = -1;
+
+        while (low <= high) {
+            // Find mid element
+            int mid = low + (high - low) / 2;
+            // If value is less than or equal to the target
+            // we will store the value in ans variable and move to the right side
+            if (arr[mid] <= target) {
+                ans = arr[mid];
+                low = mid+1;
+            }
+            // Else, we move to the left side
+            else {
+                high = mid-1;
+            }
+        }
+        return ans;
+    }
+
+    // Main Function
+    public static void main(String[] args) {
+        int[] arr = {10, 20, 30, 40, 50};
+        int target = 25;
+        int res = floor(arr, target);
+        System.out.println(res);
+    }
+}
+
+// Output -
+// 20
+
+// Floor Value Means -
+// Largest element in an array which is smaller than or equal to the target element.
+// such that : arr[idx] <= target
+
+// Algorithm : Optimal Solution
+/*
+1. Initialize low = 0, high = n-1, ans = -1
+2. Run a loop until low <= high
+3. Find mid = low + (high - low) / 2
+4. If arr[mid] >= target, then ans = arr[mid] and high = mid-1
+5. Else low = mid+1
+6. Return ans
+ */
