128cdb203fb74d5d6c82d2b62ac5971f3374b0a5
NextGenGk <atkola12345@gmail.com>
2023-08-19 21:41:35 +0530

    Add Find_Nth_Root

---
+++ BinarySearch/BS_on_Answers/Find_Nth_Root.java
@@ -0,0 +1,43 @@
+package BinarySearch.BS_on_Answers;
+
+public class Find_Nth_Root {
+
+    static int func(int mid, int n, int m) {
+        long ans = 1;
+        for (int i=1; i<=n; i++) {
+            ans = ans * mid;
+            if (ans > m) return 2;
+        }
+        if (ans == m) return 1;
+        return 0;
+    }
+
+    public static int NthRoot(int n, int m) {
+        // Use binary search on the answer space:
+        int low = 1, high = m;
+        while (low <= high) {
+            int mid = (low + high) / 2;
+            int midN = func(mid, n, m);
+            if (midN == 1) {
+                return mid;
+            } else if (midN == 0) {
+                low = mid + 1;
+            } else {
+                high = mid - 1;
+            }
+        }
+        return -1;
+    }
+
+    // Main Function
+    public static void main(String[] args) {
+        int n = 3, m = 27;
+        int ans = NthRoot(n, m);
+        System.out.println("The answer is: " + ans);
+    }
+}
+
+// Output -
+// The answer is : 3
+
+//
