1af936744b404b075f40b75f828f5281c75c6c5e
NextGenGk <atkola12345@gmail.com>
2023-08-29 12:01:41 +0530

    Update Kth_Missing_Positive_Number

--- BinarySearch/BS_on_Answers/Kth_Missing_Positive_Number.java
+++ BinarySearch/BS_on_Answers/Kth_Missing_Positive_Number.java
@@ -48,11 +48,14 @@ public class Kth_Missing_Positive_Number {
 Example 1:
 Input Format: arr[]={4,7,9,10}, k = 1
 Result: 1
-Explanation: The missing numbers are 1, 2, 3, 5, 6, 8, 11, 12, ……, and so on. Since 'k' is 1, the first missing element is 1.
+Explanation: The missing numbers are 1, 2, 3, 5, 6, 8, 11, 12, ……, and so on. Since 'k' is 1, the first missing
+element is 1.
+
 Example 2:
 Input Format: arr[]={4,7,9,10}, k = 4
 Result: 5
-Explanation: The missing numbers are 1, 2, 3, 5, 6, 8, 11, 12, ……, and so on. Since 'k' is 4, the fourth missing element is 5.
+Explanation: The missing numbers are 1, 2, 3, 5, 6, 8, 11, 12, ……, and so on. Since 'k' is 4, the fourth missing
+element is 5.
  */
 
 // Output :
