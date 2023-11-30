fbd9543551dce4fba0fb13eb59d8a7d20938cdc2
NextGenGk <atkola12345@gmail.com>
2023-11-30 22:19:23 +0530

    update Count_Digits & Reverse_Number

--- Basic_Maths/Count_Digits.java
+++ Basic_Maths/Count_Digits.java
@@ -22,3 +22,16 @@ class Count_Digits {
     }
 }
 
+// Approach
+/*
+1. Store the integer in a variable X and initialize a counter variable to count the number of digits.
+2. We know that in programming languages when we divide X by Y it will result in an integer
+(given both the variables are integers). For example, 133/10 will result in 13 similarly 1/10 will result in 0.
+3. Using a for loop and above observation keep on dividing X by 10 and increment the count in every iteration
+when X equals 0 terminate the loop and the count will have the number of digits in N.
+ */
+
+/* Time & Space Complexity
+Time Complexity: O (1)
+Space Complexity: O(1)
+ */
--- Basic_Maths/Reverse_Number.java
+++ Basic_Maths/Reverse_Number.java
@@ -20,6 +20,15 @@ public class Reverse_Number {
     }
 }
 
+// Approach
+/*
+1. Run a while loop until the given number N is equal to zero while(N!=0)
+2. a variable reverse = 0;
+3. now in each step take the remainder of the given number N and store it as a variable digit, digit = N % 10
+4. Also, Divide the number by 10. N= N / 10
+5. in each step, the variable reverse get updated as reverse = reverse*10+digit.
+ */
+
 // Time & Space Complexity -
 // Time Complexity - O(n)
 // Space Complexity - O(n)
