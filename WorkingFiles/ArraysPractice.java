adafdfaff1b2066cc2a0507390a450242de6bfb8
NextGenGk <atkola12345@gmail.com>
2023-12-24 22:38:24 +0530

    Add Next Permutation

--- WorkingFiles/ArraysPractice.java
+++ WorkingFiles/ArraysPractice.java
@@ -816,10 +816,38 @@ public class ArraysPractice {
         return arr;
     }
 
+
+    static void nextPermutation(int[] arr) {
+        int n = arr.length;
+        int idx = -1;
+        for (int i=n-2; i>=0; i--) {
+            if (arr[i] < arr[i+1]) {
+                idx = i;
+                break;
+            }
+        }
+
+        if (idx == -1) {
+            reverse(arr, 0, n-1);
+            return;
+        }
+
+        for (int i=n-1; i>idx; i--) {
+            if (arr[i] > arr[idx]) {
+                swap(arr, i, idx);
+                break;
+            }
+        }
+        reverse(arr, idx+1, n-1);
+    }
+
     public static void main(String[] args) {
-//        int n = 10, m = 7;
-        int[] arr = {1,2,-3,-1,-2,-3};
-        int[] res = rearrangev3(arr, arr.length);
-        System.out.println(Arrays.toString(res));
+        int[] arr = {2, 1, 5, 4, 3, 0, 0};
+        int n = arr.length;
+
+        nextPermutation(arr);
+
+        for (int i = 0; i < n; i++)
+            System.out.print(arr[i] + " ");
     }
 }
