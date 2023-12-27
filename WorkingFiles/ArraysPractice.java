515852dd953dc8d0420145d57a7270bc53f4075a
NextGenGk <atkola12345@gmail.com>
2023-12-27 21:28:28 +0530

    Add Rotate Matrix

--- WorkingFiles/ArraysPractice.java
+++ WorkingFiles/ArraysPractice.java
@@ -1000,23 +1000,56 @@ public class ArraysPractice {
         return matrix;
     }
 
-    public static void main(String[] args) {
-        ArrayList<ArrayList<Integer>> matrix = new ArrayList<>();
-        matrix.add(new ArrayList<>(Arrays.asList(1, 1, 1)));
-        matrix.add(new ArrayList<>(Arrays.asList(1, 0, 1)));
-        matrix.add(new ArrayList<>(Arrays.asList(1, 1, 1)));
+    static void rotateImage(int[][] matrix) {
+        int n = matrix.length;
 
-        int n = matrix.size();
-        int m = matrix.get(0).size();
+        int[][] ansMatrix = new int[n][n];
 
-        ArrayList<ArrayList<Integer>> ans = setMatrixv1(matrix, n, m);
+        for (int i=0; i<n; i++) {
+            for (int j=0; j<n; j++) {
+                ansMatrix[j][(n-1)-i] = matrix[i][j];
+            }
+        }
 
-        System.out.println("The Final matrix is: ");
-        for (ArrayList<Integer> row : ans) {
-            for (Integer ele : row) {
-                System.out.print(ele + " ");
+        for (int i=0; i<n; i++) {
+            for (int j=0; j<n; j++) {
+                System.out.print(ansMatrix[i][j] + " ");
             }
             System.out.println();
         }
+    }
+
+    static void rotateImagev2(int[][] matrix) {
+        int n = matrix.length;
+
+        for (int i=0; i<n; i++) {
+            for (int j=i; j<n; j++) {
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = temp;
+            }
+        }
+
+        for (int i=0; i<n; i++) {
+            for (int j=0; j<n/2; j++) {
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[i][(n-1)-j];
+                matrix[i][(n-1)-j] = temp;
+            }
+        }
+
+        for (int i=0; i<n; i++) {
+            for (int j=0; j<n; j++) {
+                System.out.print(matrix[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) {
+        int[][] matrix = {{1,2,3},
+                {4,5,6},
+                {7,8,9}};
+        rotateImagev2(matrix);
     }
 }
