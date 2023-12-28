eb62aad2a62d2c55aaec30413309faea71620ca1
NextGenGk <atkola12345@gmail.com>
2023-12-28 20:33:42 +0530

    Add Spiral Matrix

--- WorkingFiles/ArraysPractice.java
+++ WorkingFiles/ArraysPractice.java
@@ -1046,10 +1046,51 @@ public class ArraysPractice {
         }
     }
 
+    static List<Integer> spiralOrder(int[][] matrix) {
+
+        List<Integer> ans = new ArrayList<>();
+
+        int n = matrix.length;
+        int m = matrix[0].length;
+
+        int left = 0;
+        int right = n-1;
+        int top = 0;
+        int bottom = n-1;
+
+        while (left <= right && top <= bottom) {
+            for (int i=left; i<= right; i++) {
+                ans.add(matrix[top][i]);
+            }
+            top++;
+
+            for (int i=top; i<= bottom; i++) {
+                ans.add(matrix[i][right]);
+            }
+            right--;
+
+            if (top <= bottom) {
+                for (int i=right; i>=left; i--){
+                    ans.add(matrix[bottom][i]);
+                }
+                bottom--;
+            }
+
+            if (left <= right) {
+                for (int i=bottom; i>=top; i--){
+                    ans.add(matrix[i][left]);
+                }
+                left++;
+            }
+        }
+        return ans;
+    }
+
     public static void main(String[] args) {
-        int[][] matrix = {{1,2,3},
-                {4,5,6},
-                {7,8,9}};
-        rotateImagev2(matrix);
+        int[][] matrix = {{1, 2, 3},
+                {4, 5, 6},
+                {7, 8, 9}};
+        List<Integer> ans = spiralOrder(matrix);
+        System.out.println(ans);
     }
 }
