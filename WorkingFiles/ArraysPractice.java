9240b1636ad97c8ca71df78e2e2d055cb743c09d
NextGenGk <atkola12345@gmail.com>
2024-01-11 22:58:41 +0530

    Add Arrays Practice - Merge Two Sorted Array Without Using Extra Space

--- WorkingFiles/ArraysPractice.java
+++ WorkingFiles/ArraysPractice.java
@@ -1523,10 +1523,78 @@ public class ArraysPractice {
         return ans;
     }
 
-    // Main Function
+
+
+    static void merge(long[] arr1, long[] arr2, int n, int m) {
+        int left = n-1;
+        int right = 0;
+
+        while (left >= 0 && right < m) {
+            if (arr1[left] > arr2[right]) {
+                long temp = arr1[left];
+                arr1[left] = arr2[right];
+                arr2[right] = temp;
+                left--;
+                right++;
+            }
+            else {
+                break;
+            }
+        }
+        Arrays.sort(arr1);
+        Arrays.sort(arr2);
+    }
+
+    private static void swapIfGreater(long[] arr1, long[] arr2, int left, int right) {
+        if (arr1[left] > arr2[right]) {
+            long temp = arr1[left];
+            arr1[left] = arr2[right];
+            arr2[right] = temp;
+        }
+    }
+
+    public static void merge2(long[] arr1, long[] arr2, int n, int m) {
+        int len = n + m;
+        int gap = (len / 2) + (len % 2);
+
+        while (gap > 0) {
+            int left = 0;
+            int right = left + gap;
+
+            while (right < len) {
+                // left pointer is on left & right pointers is on right
+                if (left < n && right >= n) {
+                    swapIfGreater(arr1, arr2, left, right - n);
+                }
+                // both pointers in arr2
+                else if (left >= n) {
+                    swapIfGreater(arr2, arr2, left-n, right-n);
+                }
+                else {
+                    swapIfGreater(arr1, arr1, left, right);
+                }
+                left++;
+                right++;
+            }
+            if (gap == 1) break;
+            gap = (gap / 2) + (gap % 2);
+        }
+    }
+
     public static void main(String[] args) {
-        int[][] arr = {{1,3}, {2,6}, {8,10}, {15,18}};
-        List<List<Integer>> ans = mergeIntervals2(arr);
-        System.out.println(ans);
+        long[] arr1 = {1, 4, 8, 10};
+        long[] arr2 = {2, 3, 9};
+        int n = 4, m = 3;
+        merge2(arr1, arr2, n, m);
+        System.out.println("The merged arrays are:");
+        System.out.print("arr1[] = ");
+        for (int i = 0; i < n; i++) {
+            System.out.print(arr1[i] + " ");
+        }
+        System.out.print("\narr2[] = ");
+        for (int i = 0; i < m; i++) {
+            System.out.print(arr2[i] + " ");
+        }
+        System.out.println();
     }
 }
