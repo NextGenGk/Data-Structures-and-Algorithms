73ea9ce58a79650820412a0a8fcb6a8262d82a41
NextGenGk <atkola12345@gmail.com>
2024-01-16 21:48:03 +0530

    Add Binary Practice - Lower Bound & Upper Bound

---
+++ BinaryPractice.java
@@ -0,0 +1,60 @@
+public class BinaryPractice {
+
+    // Implement lower bound
+    static int lowerBound(int[] arr, int target) {
+        int n = arr.length;
+
+        for (int i=0; i<n; i++) {
+            if (arr[i] >= target) {
+                return i;
+            }
+        }
+        return n;
+    }
+
+    static int lowerBound2(int[] arr, int target) {
+        int n = arr.length;
+        int ans = n;
+
+        int low = 0;
+        int high = n-1;
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+            if (arr[mid] >= target) {
+                ans = mid;
+                high = mid-1;
+            }
+            // do not alter tha answer because this
+            // doesn't have a probability to be my answer
+            else low = mid + 1;
+        }
+        return ans;
+    }
+
+    // Implement upper bound
+    static int upperBound2(int[] arr, int target) {
+        int n = arr.length;
+        int ans = n;
+
+        int low = 0;
+        int high = n-1;
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+            if (arr[mid] > target) {
+                ans = mid;
+                high = mid-1;
+            }
+            // do not alter tha answer because this
+            // doesn't have a probability to be my answer
+            else low = mid + 1;
+        }
+        return ans;
+    }
+
+    // Main Function
+    public static void main(String[] args) {
+        int[] arr = {3,5,8,15,19};
+        int ans = lowerBound(arr, 15);
+        System.out.println(ans);
+    }
+}
