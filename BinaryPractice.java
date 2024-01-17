fbbfb20d634a8d90e334dd1fd8f74bc377727a26
NextGenGk <atkola12345@gmail.com>
2024-01-17 23:16:26 +0530

    Add Binary Practice - Search Insert Position

--- BinaryPractice.java
+++ BinaryPractice.java
@@ -51,6 +51,39 @@ public class BinaryPractice {
         return ans;
     }
 
+    // Search Insert Position
+    // Brute
+    public int searchInsert(int[] nums, int target) {
+        int n = nums.length;
+        for (int i=0; i<n; i++) {
+            if (nums[i] >= target) {
+                return i;
+            }
+        }
+        return n;
+    }
+
+    // Optimal
+    public int searchInsert2(int[] nums, int target) {
+        int n = nums.length;
+        int ans = n;
+        int low = 0;
+        int high = n-1;
+
+        while (low <= high) {
+            int mid = (low + high) / 2;
+
+            if (nums[mid] >= target) {
+                ans = mid;
+                high = mid - 1;
+            }
+            else {
+                low = mid + 1;
+            }
+        }
+        return ans;
+    }
+
     // Main Function
     public static void main(String[] args) {
         int[] arr = {3,5,8,15,19};
