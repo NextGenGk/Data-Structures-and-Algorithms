2599727566baec00154562771812a068b197f2d4
NextGenGk <atkola12345@gmail.com>
2024-01-20 23:16:19 +0530

    Add Binary Practice - Count Occurrences in a Sorted Array, Search Element in Rotated Sorted Array

--- BinaryPractice.java
+++ BinaryPractice.java
@@ -141,6 +141,81 @@ public class BinaryPractice {
         return new int[]{lb, upperBound2(arr, k) - 1};
     }
 
+    // Count occurrences in a sorted array
+    public static int firstOccurrence(int[] arr, int n, int k) {
+        int low = 0, high = n - 1;
+        int first = -1;
+
+        while (low <= high) {
+            int mid = (low + high) / 2;
+            if (arr[mid] == k) {
+                first = mid;
+                high = mid - 1;
+            } else if (arr[mid] < k) {
+                low = mid + 1;
+            } else {
+                high = mid - 1;
+            }
+        }
+        return first;
+    }
+
+    public static int lastOccurrence(int[] arr, int n, int k) {
+        int low = 0, high = n - 1;
+        int last = -1;
+
+        while (low <= high) {
+            int mid = (low + high) / 2;
+            if (arr[mid] == k) {
+                last = mid;
+                low = mid + 1;
+            } else if (arr[mid] < k) {
+                low = mid + 1;
+            } else {
+                high = mid - 1;
+            }
+        }
+        return last;
+    }
+
+    public static int count(int arr[], int n, int x) {
+        int first = firstOccurrence(arr, n, x);
+        if (first == -1) return 0;
+
+        int last = lastOccurrence(arr, n, x);
+        return (last - first) + 1;
+    }
+
+    // Search in rotated sorted array
+    public int search(int[] nums, int target) {
+        int low = 0;
+        int high = nums.length-1;
+
+        while (low <= high) {
+            int mid = (low + high) / 2;
+
+            if (nums[mid] == target) return mid;
+
+            if(nums[low] <= nums[mid]) {
+                if (nums[low] <= target && target <= nums[mid]) {
+                    high = mid - 1;
+                }
+                else {
+                    low = mid + 1;
+                }
+            }
+            else {
+                if (nums[mid] <= target && target <= nums[high]) {
+                    low = mid + 1;
+                }
+                else {
+                    high = mid - 1;
+                }
+            }
+        }
+        return -1;
+    }
+
     // Main Function
     public static void main(String[] args) {
         int[] arr = {2, 4, 6, 8, 8, 8, 11, 13};
