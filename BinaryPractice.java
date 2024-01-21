95ba59adc54526572b77142f666e61647eb8a8fd
NextGenGk <atkola12345@gmail.com>
2024-01-21 23:20:15 +0530

    Add Binary Practice - Search Element in Rotated Sorted Array II

--- BinaryPractice.java
+++ BinaryPractice.java
@@ -216,6 +216,44 @@ public class BinaryPractice {
         return -1;
     }
 
+    // Search in Rotated Sorted Array II
+    public boolean search2(int[] nums, int target) {
+        int low = 0;
+        int high = nums.length - 1;
+
+        while (low <= high) {
+            int mid = (low + high) / 2;
+
+            if (nums[mid] == target)
+                return true;
+
+            // if both parts are sorted
+            if (nums[low] == nums[mid] && nums[mid] == nums[high]) {
+                low = low + 1;
+                high = high - 1;
+                continue;
+            }
+
+            if (nums[low] <= nums[mid]) {
+                if (nums[low] <= target && target <= nums[mid]) {
+                    high = mid - 1;
+                }
+                else {
+                    low = mid + 1;
+                }
+            }
+            else {
+                if (nums[mid] <= target && target <= nums[high]) {
+                    low = mid + 1;
+                }
+                else {
+                    high = mid - 1;
+                }
+            }
+        }
+        return false;
+    }
+
     // Main Function
     public static void main(String[] args) {
         int[] arr = {2, 4, 6, 8, 8, 8, 11, 13};
