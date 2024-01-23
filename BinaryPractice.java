3fbc45596384921b0441586fc5836eeb3af7cddd
NextGenGk <atkola12345@gmail.com>
2024-01-23 22:11:56 +0530

    Add Binary Practice - Find out how many times has been rotated

--- BinaryPractice.java
+++ BinaryPractice.java
@@ -281,6 +281,42 @@ public class BinaryPractice {
         return ans;
     }
 
+    // Find ot how many times the array has been rotated
+    public static int findKRotation(int []arr){
+        // Write your code here.
+        int low = 0;
+        int high = arr.length-1;
+        int ans = Integer.MAX_VALUE;
+        int index = -1;
+
+        while (low <= high) {
+            int mid = (low + high) / 2;
+
+            if (arr[low] <= arr[high]) {
+                if (arr[low] < ans) {
+                    index = low;
+                    ans = arr[low];
+                }
+            }
+
+            if (arr[low] <= arr[mid]) {
+                if (arr[low] < ans) {
+                    index = low;
+                    ans = arr[low];
+                }
+                low = mid + 1;
+            }
+            else {
+                if (arr[mid] <= arr[high]) {
+                    index = mid;
+                    ans = arr[mid];
+                }
+                high = mid - 1;
+            }
+        }
+        return index;
+    }
+
     // Main Function
     public static void main(String[] args) {
         int[] arr = {2, 4, 6, 8, 8, 8, 11, 13};
