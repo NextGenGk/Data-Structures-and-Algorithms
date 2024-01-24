a771f49ad2e6fd05380aa0cedf04a84fdcf83676
NextGenGk <atkola12345@gmail.com>
2024-01-24 23:00:39 +0530

    Add Binary Practice - Find Single Element

--- BinaryPractice.java
+++ BinaryPractice.java
@@ -317,12 +317,34 @@ public class BinaryPractice {
         return index;
     }
 
+    static int singleElement1(int[] arr) {
+        int n = arr.length;
+
+        if (n == 1) return arr[0];
+        if (arr[0] != arr[1]) return arr[0];
+        if (arr[n-1] != arr[n-2]) return arr[n-1];
+
+        int low = 1;
+        int high = n-2;
+
+        while (low <= high) {
+            int mid = (low + high) / 2;
+
+            if (arr[mid] != arr[mid-1] && arr[mid] != arr[mid+1]) return arr[mid];
+            if ((mid % 2 == 1) && arr[mid] == arr[mid-1] || (mid % 2 == 0) && arr[mid] == arr[mid+1]) {
+                low = mid + 1;
+            }
+            else {
+                high = mid - 1;
+            }
+        }
+        return -1;
+    }
+
     // Main Function
     public static void main(String[] args) {
-        int[] arr = {2, 4, 6, 8, 8, 8, 11, 13};
-        int n = 8, k = 8;
-        int[] ans = firstAndLastPosition2(arr, n, k);
-        System.out.println("The first and last positions are: "
-                + ans[0] + " " + ans[1]);
+        int[] arr = {1,1,2,2,3,3,4,5,5,6,6};
+        int res = singleElement1(arr);
+        System.out.println(res);
     }
 }
