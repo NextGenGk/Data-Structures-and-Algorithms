700fc174713dba92d3d2143ed62eeb3b9a12e2e8
NextGenGk <atkola12345@gmail.com>
2024-01-27 23:08:29 +0530

    Add BS on Answers Practice - Find Nth Root

--- BS_on_Answers_Practice.java
+++ BS_on_Answers_Practice.java
@@ -17,4 +17,32 @@ public class BS_on_Answers_Practice {
         }
         return (int) high;
     }
+
+    // Find Nth Root
+    public static int NthRoot(int n, int m) {
+        // Write your code here.
+        int low = 0;
+        int high = m;
+
+        while (low <= high) {
+            int mid = (low + high) / 2;
+            if (findMidPowerM(mid, n, m) == 1) {
+                return mid;
+            }
+            else if (findMidPowerM(mid, n, m) == 2) high = mid - 1;
+            else low = mid + 1;
+        }
+        return -1;
+    }
+
+    // Helper function to find mid^n
+    public static int findMidPowerM(int mid, int n, int m) {
+        long ans =  1;
+        for (int i=1; i<=n; i++) {
+            ans = ans * mid;
+            if (ans > m) return 2;
+        }
+        if (ans == m) return 1;
+        else return 0;
+    }
 }
